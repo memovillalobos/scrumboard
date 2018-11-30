7a0ee9003fdadf238ec8e257c102cd4447ab0025
Guillermo Villalobos <guillermo@pixan.io>
2018-11-30 10:11:04 -0600

    Layout inicial

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="Header">
+          <h1>My Sprint</h1>
+        </div>
+        <div className="LaneContainer">
+          <div className="Lane">To Do</div>
+          <div className="Lane">In Progress</div>
+          <div className="Lane">Done</div>
+        </div>
       </div>
     );
   }
